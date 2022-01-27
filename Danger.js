aec68dc3158a9b20855408dfefafa5339e12638d
Sufgit12345678910 <98509162+Sufgit12345678910@users.noreply.github.com>
2022-01-27 10:59:28 +0300

    Add files via upload

---
+++ Danger.js
@@ -0,0 +1,36 @@
+document.querySelector("#1").addEventListener("click", () => {
+none(1);
+});
+
+document.querySelector("#2").addEventListener("click", () => {
+  none(2);
+});
+
+document.querySelector("#3").addEventListener("click", () => {
+none(3);
+});
+
+document.querySelector("#4").addEventListener("click", () => {
+  none(4);
+});
+document.querySelector("#5").addEventListener("click", () => {
+none(5);
+});
+
+document.querySelector("#6").addEventListener("click", () => {
+  none(6);
+});
+document.querySelector("#7").addEventListener("click", () => {
+none(7);
+});
+
+document.querySelector("#8").addEventListener("click", () => {
+  none(8);
+});
+document.querySelector("#9").addEventListener("click", () => {
+none(9);
+});
+
+document.querySelector("#10").addEventListener("click", () => {
+  none(10);
+});
